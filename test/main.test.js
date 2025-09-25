c7dff46178eda45269e7012037dbc8cec53f83a8
dhyey178 <dhyeya.shah178@gmail.com>
2025-09-25 13:17:36 +0530

    feat(deliveryCost): add failing test for end to end input processing

---
+++ test/main.test.js
@@ -0,0 +1,22 @@
+const { processInput } = require('../src/index');
+
+describe('End-to-End Input Processing', () => {
+  test('should process sample input and return correct costs and discounts for all packages', () => {
+    const inputLines = [
+      '100 3',
+      'PKG1 5 5 OFR001',
+      'PKG2 15 5 OFR002',
+      'PKG3 10 100 OFR003',
+    ];
+
+    const expectedOutput = [
+      'PKG1 0 175',
+      'PKG2 0 275',
+      'PKG3 35 665',
+    ];
+
+    const actualOutput = processInput(inputLines);
+
+    expect(actualOutput).toEqual(expectedOutput);
+  });
+});

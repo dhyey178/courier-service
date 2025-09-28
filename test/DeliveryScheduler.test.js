6f971273952ceba29cd08992ebb263394f705b3d
dhyey178 <dhyeya.shah178@gmail.com>
2025-09-28 10:05:31 +0530

    feat(deliveryTime): add failing test case for minimizing delivery time

--- test/DeliveryScheduler.test.js
+++ test/DeliveryScheduler.test.js
@@ -22,6 +22,9 @@ describe('DeliveryScheduler Priority 1: Max Package Count', () => {
 
     expect(maxCount).toBe(5); 
   });
+});
+
+describe('DeliveryScheduler Priority 2: Maximize Total Weight', () => {
 
   test('should find the group of size count that has the maximum possible weight', () => {
     const scheduler = new DeliveryScheduler(1, 200, 70); 
@@ -67,4 +70,23 @@ describe('DeliveryScheduler Priority 1: Max Package Count', () => {
     expect(tripCandidates[0].reduce((sum, p) => sum + p.weight, 0)).toBe(200);
     expect(tripCandidates[1].reduce((sum, p) => sum + p.weight, 0)).toBe(200);
   });
-}); 
+});
+
+describe('DeliveryScheduler Priority 3: Minimize Delivery Time (Min Distance)', () => {
+  test('should select the single trip from candidates that has the minimum maximum distance', () => {
+    const scheduler = new DeliveryScheduler(1, 200, 70); 
+    const candidates = [
+      [new Package('P_A1', 125, 100, '', 100), new Package('P_A2', 75, 50, '', 100)],
+      [new Package('P_B1', 100, 75, '', 100), new Package('P_B2', 100, 10, '', 100)], 
+      [new Package('P_C1', 110, 120, '', 100), new Package('P_C2', 90, 10, '', 100)], 
+    ];
+
+    const finalTrip = scheduler.selectBestTripByTime(candidates);
+
+    const finalTripIds = finalTrip.map(p => p.id).sort();
+    expect(finalTripIds).toEqual(['P_B1', 'P_B2']);
+
+    const maxDistance = finalTrip.reduce((max, p) => Math.max(max, p.distance), 0);
+    expect(maxDistance).toBe(75); 
+  })
+});
